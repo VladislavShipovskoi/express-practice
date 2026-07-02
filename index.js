160712a73ae413d627a2bd675c7dbae20f211e7b
Vladislav Shipovskoy <vladislav.shipovskoy@gmail.com>
2026-07-02 12:44:15 +0700

    add error middleware

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const { logger, error404 } = require("./middleware");
+const { logger, error404, error } = require("./middleware");
 const books = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.set("view engine", "ejs");
 app.use(logger);
 app.use("/", books);
 app.use(error404);
+app.use(error);
 
 app.listen(PORT, () => {
   console.log(`Library app listening on port ${PORT}`);
