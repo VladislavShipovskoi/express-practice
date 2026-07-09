73c4c92e36abf9196cd5b8c378d667636a4d5385
Vladislav Shipovskoy <vladislav.shipovskoy@gmail.com>
2026-07-09 12:34:12 +0700

    use count service in library app

--- middleware/file.js
+++ middleware/file.js
@@ -5,7 +5,6 @@ const storage = multer.diskStorage({
     cb(null, "public/img");
   },
   filename: function (req, file, cb) {
-    console.log(req);
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
