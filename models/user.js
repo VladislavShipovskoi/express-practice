8e79fdd2d4ec8e0136d4b8b1c2dd10ade424c5c4
Vladislav Shipovskoy <vladislav.shipovskoy@gmail.com>
2026-08-11 15:17:51 +0700

    add user schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose").default;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = model("User", UserSchema);
